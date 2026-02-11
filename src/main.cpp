4e8a4fcdd8457ba8145804fe57615d2ba9e9f6de
lavany108 <lavanya.gla_cs.aiml24@gla.ac.in>
2026-02-11 09:31:51 +0530

    Added Adafruit OLED Screen dimensions and address in main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_ADDR 0x3C
 void setup() {
 // write your initialization code here
 }
